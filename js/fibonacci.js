10367bbb4816a5c86af3bdaf787da660a640c772
Crusader-RG <justin.leeper92@gmail.com>
2024-05-27 09:11:08 -0600

    completed

--- js/fibonacci.js
+++ js/fibonacci.js
@@ -1,5 +1,11 @@
 const fibonacci = (num) => {
-    
+    let initArray = [0,1]
+    if (num > 1) {
+    for (let i = 2 ; i <= num; i++) {
+        initArray.push(initArray[i-1]+initArray[i-2])
+    }
+}
+return initArray[num] 
 }
 
 module.exports = {fibonacci}
